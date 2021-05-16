48480413fd88e67f6ecfbe4875df93461712d969
anahis0l <anicastillolara@gmail.com>
2021-05-16 17:31:46 -0300

    Jquery desafio

--- js/main.js
+++ js/main.js
@@ -20,8 +20,8 @@ baseDatosProductos.push(new Producto ('Cepillo Dientes - Bambu',  250, 'img1','C
 baseDatosProductos.push(new Producto('Hilo dental Vegano',  450,'img2', 'Cuidado Dental', 2));
 baseDatosProductos.push (new Producto('Pasta dental Ayurvedica',  350,'img3','Cuidado Dental',4));
 baseDatosProductos.push (new Producto ('Shampoo Sólido',  580, 'img4','Cuidado del pelo', 2));
-baseDatosProductos.push (new Producto('Acondicionador Sólido',  590,'img5','Cuidado Dental', 3));
-baseDatosProductos.push (new Producto ('Jabón Vegeano', 390, 'img6', 'Cuidado de la piel', 4));
+baseDatosProductos.push (new Producto('Acondicionador Sólido',  590,'img5','Cuidado del pelo', 3));
+baseDatosProductos.push (new Producto ('Jabón Vegeano', 390, 'img6', 'Cuidado del pelo', 4));
 baseDatosProductos.push (new Producto ('Copita Menstrual', 1500, 'img7', 'Cuidado Femenino' ,3));
 baseDatosProductos.push (new Producto ('Agua de rosas', 750, 'img8','Cuidado Femenino' ,2));
 baseDatosProductos.push (new Producto ('Pads desmaquillantes',  720,'img9','Cuidado Femenino',2));
@@ -55,7 +55,7 @@ $("#productos").html(acumulador);
                       <option value="2">2</option>
                       <option value="3">3</option>
                   </select>
-                <i class="text-right"> Stock: ${baseDatosProductos.stock}</i>
+                <i> Stock : ${baseDatosProductos.stock}</i>
                 </div>    
                 <div class="card-footer">
                 <button class="rounded comprar" onclick="agregarAlCarrito('${baseDatosProductos.precio}')"><i class="icon-cart"></i> Agregar al carrito</button>
@@ -68,34 +68,12 @@ $("#productos").html(acumulador);
 
       }
 
-// Bienvenida y pedir e-mail  ---- esta ok 
 
-// let bienvenidos = prompt ('Deseas suscribirte a nuestro Newsletter? Escribe si o no');
-// let emailSi = "si"
-// let emailNo = "no"
-//   if (bienvenidos==emailSi){
-//     let email =  prompt ('Dejanos tu E-mail, para recibir todas nuestras Ofertas!');
-//     alert ('Gracias por suscribirte! te enviaremos un mail a tu casilla: ' + email );
-
-//     const emailUsuario = email;
-//     localStorage.setItem( 'e-mail del usuario', email );
-
-//     const usuarioLocal = localStorage.getItem ('e-mail del usuario');
-//     console.log (usuarioLocal);
-//   }
- 
-// Saludo personalizado -- esta ok
-
-// const titulo = document.getElementById('titulo')
-// const usuario = prompt('Hola, ingrese su nombre')
-// titulo.innerText = `¡Bienvenido ${usuario}!` 
-
-
-const selectNum = document.getElementById('cantidad');
 let carrito = [];
+const unidades = document.getElementById('cantidad').value
 
 function agregarAlCarrito (precio) {
-  let inputValue = selectNum.value
+  let inputValue = unidades.value
   let productoElegido = baseDatosProductos.find( el => el.precio == precio )
   if ( inputValue == '1'){   
      console.log('Se agrego un nuevo producto al carrito. El total es: $ '+  precio)
@@ -111,47 +89,39 @@ function agregarAlCarrito (precio) {
       console.log (carrito);
 
       function sumarIva (precio) {
-        return selectNum.value =(precio * 1.21) }
+        return unidades.value =(precio * 1.21) }
         console.log ('El precio del producto seleccionado + iva es $: ' + sumarIva (precio * 1.21) + ' por unidad');
    
         actualizarCarrito()   
 }
 
 
-document.getElementById('cantidad').addEventListener('input', stockCantidad);
 
-function stockCantidad(event){
-  console.log(event)
-  console.log(event.target.value)
-let unidades = document.getElementById('cantidad').value;
-    if (unidades < baseDatosProductos.stock){
-    console.loge ("Tenemos stock");
-    } else {
-    console.log ("No tenemos stock suficiente"); 
-  }
-}
+
  
 /// funciones para filtrar 
-document.getElementById('todos').addEventListener('click', botonTodos);
-function botonTodos() {
-      mostrarProductos(baseDatosProductos);
 
-}
-document.getElementById('dental').addEventListener('click', botonDental);
-function botonDental() {
-  let valorDental = baseDatosProductos.categoria
-  if (valorDental == 'Cuidado Dental' ) { 
-      mostrarProductos( baseDatosProductos.filter( el => el.categoria == 'Cuidado Dental') );
-    }
-}
+$('#todos').on('click', () => {
+  mostrarProductos(array);
+})
 
-document.getElementById('pelo').addEventListener('click', botonPelo);
-function botonPelo(categoria) {
-  let valorDental = baseDatosProductos.categoria
-  if (categoria == valorDental) { 
-      mostrarProductos( baseDatosProductos.filter( el => el.categoria == 'Cuidado del pelo') );
-    }
-}
+$('#dental').on('click', () => {
+  const valorDental = baseDatosProductos.filter( el => el.categoria == 'Cuidado Dental');
+  console.log(valorDental)
+  return mostrarProductos(valorDental)
+})
+
+$('#pelo').on('click', () => {
+  const valorPelo = baseDatosProductos.filter( el => el.categoria == 'Cuidado del pelo');
+  console.log(valorPelo)
+  mostrarProductos(valorPelo)
+})
+
+$('#fem').on('click', () => {
+  const valorFem = baseDatosProductos.filter( el => el.categoria == 'Cuidado Femenino');
+  console.log(valorFem)
+  mostrarProductos(valorFem)
+})
 
 
 
--- js/modal.js
+++ js/modal.js
@@ -11,6 +11,13 @@ botonCerrar.addEventListener('click', ()=>{
 })
 
 
+// const botonEliminar = document.getElementById('remove')
+
+// botonEliminar.addEventListener('click', ()=>{
+//     contenedorCarrito.classList.remove ('productoEnCarrito')
+
+// })
+
 
 // modal funcion
 const contenedorCarrito = document.getElementById('carrito-contenedor')
@@ -23,9 +30,25 @@ function actualizarCarrito (){
 
   const div = document.createElement('div')
   div.classList.add('productoEnCarrito')
-  div.innerHTML = `<p>${selectNum.value}</p>
-                  <p>${baseDatosProductos.nombre}</p>
-                  <p>${baseDatosProductos.precio}</p>                 
+  div.innerHTML = `
+                <div class="container">
+                    <div class="row">
+                        <div class="col-1">
+                            <p>${unidades.value}</p>
+                        </div>
+                        <div class="col-8">
+                            <h5>${baseDatosProductos.nombre}</h5>
+
+                            <i>$${baseDatosProductos.precio}</i> 
+                            </div>
+                        <div class="col-2">
+                            <img class="animate__animated animate__fadeIn" src="./imagenes/${baseDatosProductos.imagen}.jpg" alt="" with="100px" height="100px"> 
+                        </div>
+                            <div class="col-1 d-flex align-items-center">
+                                <i class="icon-remove " id="remove"></i>
+                            </div>
+                        </div>
+                </div>          
                  `
                  contenedorCarrito.appendChild(div) 
                 })
